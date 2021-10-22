f791190f35b6d9e278a68124bf712d2120a079c4
kimjunsoo <rlawnstn01023@naver.com>
2021-10-22 19:13:31 +0900

    Initial Commit

---
+++ ConnetJson_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConnetJson_Demo
+//
+//  Created by 준수김 on 2021/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
